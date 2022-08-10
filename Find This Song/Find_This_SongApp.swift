f314fb7d16942f0696254422cf08217c871961c7
Bartosz Klimek <klimekbartosz12@gmail.com>
2022-08-10 10:24:47 +0100

    Initial Commit

---
+++ Find This Song/Find_This_SongApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_This_SongApp.swift
+//  Find This Song
+//
+//  Created by Bartosz Klimek on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Find_This_SongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Find This SongUITests/Find_This_SongUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Find_This_SongUITestsLaunchTests.swift
+//  Find This SongUITests
+//
+//  Created by Bartosz Klimek on 10/08/2022.
+//
+
+import XCTest
+
+class Find_This_SongUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
